6db6aa86324e3d7b02f3e9f9a8ba1aaf5892c47a
unknown <bryantdalton19@gmail.com>
2023-10-25 11:03:39 -0400

    BeautifulSoup

--- scrape.py
+++ scrape.py
@@ -3,4 +3,5 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/news')
 
-print(res)
+soup = BeautifulSoup(res.text, 'html.parser')
+print(soup.find(id='score_38012032'))
