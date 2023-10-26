f9c89abda47460a5f76b1006d3f25cb2b91376aa
unknown <bryantdalton19@gmail.com>
2023-10-26 10:11:31 -0400

    custom hn links

--- scrape.py
+++ scrape.py
@@ -4,4 +4,20 @@ from bs4 import BeautifulSoup
 res = requests.get('https://news.ycombinator.com/news')
 
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.find(id='score_38012032'))
+links = soup.select('.titleline > a')
+subtext= soup.select('.subtext')
+
+
+def create_custom_hn(links, subtext): 
+    hn= []
+    for idx, item in enumerate(links):
+        title= links[idx].getText()
+        href= links[idx].get('href', None)
+        vote= subtext[idx].select('.score')
+        if len(vote): 
+            points= int(vote[0].getText('', None).replace(' points', ''))
+            print(points)
+            hn.append({'title':title, 'link':href})
+    return hn
+
+create_custom_hn(links, subtext)
