ce1f68c045e9219ee409297bd3918e85623aa34e
unknown <bryantdalton19@gmail.com>
2023-10-29 12:13:59 -0400

    removed irrelevant code

--- scrape.py
+++ scrape.py
@@ -12,7 +12,7 @@ def scrape_page(i):
         res = requests.get('https://news.ycombinator.com/news')
     else:
         res = requests.get(f'https://news.ycombinator.com/news?p={i}')
-
+    #grab relevant data
     soup = BeautifulSoup(res.text, 'html.parser')
     links = soup.select('.titleline > a')
     subtext= soup.select('.subtext')
@@ -37,38 +37,3 @@ page_num = input('How many pages? ')
 #print page data
 for i in range(int(page_num)):
     pprint.pprint(scrape_page(i + 1))
-
-
-
-
-exit()
-res = requests.get('https://news.ycombinator.com/news')
-res2 = requests.get('https://news.ycombinator.com/news?p=2')
-
-soup = BeautifulSoup(res.text, 'html.parser')
-links = soup.select('.titleline > a')
-subtext= soup.select('.subtext')
-
-soup2 = BeautifulSoup(res2.text, 'html.parser')
-links2 = soup2.select('.titleline > a')
-subtext2= soup2.select('.subtext')
-
-mega_links= links + links2
-mega_sub= subtext + subtext2
-
-def sort_by_votes(hnlist):
-    return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
-
-def create_custom_hn(links, subtext): 
-    hn= []
-    for idx, item in enumerate(links):
-        title= links[idx].getText()
-        href= links[idx].get('href', None)
-        vote= subtext[idx].select('.score')
-        if len(vote): 
-            points= int(vote[0].getText('', None).replace(' points', ''))
-            if points > 99:  
-                hn.append({'title':title, 'link':href, 'votes': points})
-    return sort_by_votes(hn)
-
-pprint.pprint(create_custom_hn(mega_links, mega_sub))
