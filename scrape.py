d7fa38ba1d2ae038f62cbdeb5e3b2e6cb4ecba4c
unknown <bryantdalton19@gmail.com>
2023-10-29 12:08:53 -0400

    pagination functionality and limit to 100 votes

--- scrape.py
+++ scrape.py
@@ -7,32 +7,39 @@ plist = []
 res_list = []
 
 def scrape_page(i):
-    
+    #preconstruct urls
     if i == 1:
         res = requests.get('https://news.ycombinator.com/news')
     else:
-        res = requests.get(f'https://news.ycombinator.com/news?p={i}') 
+        res = requests.get(f'https://news.ycombinator.com/news?p={i}')
+
+    soup = BeautifulSoup(res.text, 'html.parser')
+    links = soup.select('.titleline > a')
+    subtext= soup.select('.subtext')
+
+    hn= []
+    for idx, item in enumerate(links):
+        title= links[idx].getText()
+        href= links[idx].get('href', None)
+        vote= subtext[idx].select('.score')
+        if len(vote): 
+            points= int(vote[0].getText('', None).replace(' points', ''))
+            if points > 99:  
+                hn.append({'title':title, 'link':href, 'votes': points})
+    return hn
 
 
 
 
 #input num pages in terminal
 page_num = input('How many pages? ')
-#preconstruct urls
-for i in range(int(page_num)):
-    scrape_page(i + 1)
-#     if i == 0:
-#         plist.append('https://news.ycombinator.com/news')
-#     else:
-#         plist.append(f'https://news.ycombinator.com/news?p={i + 1 }')
-#request urls
-for item in plist:
-    res_list.append(requests.get(item))
-
-print(res_list[0].text)
-#combine page data
 
 #print page data
+for i in range(int(page_num)):
+    pprint.pprint(scrape_page(i + 1))
+
+
+
 
 exit()
 res = requests.get('https://news.ycombinator.com/news')
